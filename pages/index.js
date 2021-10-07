eae1dc785df91fd106e8b5bdcbfeb11ffa141972
R0bl0x10501050 <R0bl0x10501050@gmail.com>
2021-10-06 19:43:45 -0700

    fixed directory

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,3 @@
-function Index(props) {
-	return (
-		<>
-			<pre style="word-wrap: break-word; white-space: pre-wrap; font-family: monospace;">
-				<p>{props.message}</p>
-			</pre>
-		</>
-	)
+export default function Index() {
+	return (<></>);
 }
-
-export async function getServerSideProps(ctx) {
-	ctx.res.json({ message: 'Welcome to the RBX Cloud API!' });
-	return {
-		props: {
-			message: "Welcome to the RBX Cloud API!"
-		}, // will be passed to the page component as props
-	}
-}
-
-export default Index;
