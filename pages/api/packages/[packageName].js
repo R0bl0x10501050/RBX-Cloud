580c7610568c624eef23b78cad80b110c54b6848
R0bl0x10501050 <R0bl0x10501050@gmail.com>
2021-10-06 21:26:52 -0700

    using `url` now

--- pages/api/packages/[packageName].js
+++ pages/api/packages/[packageName].js
@@ -2,8 +2,7 @@ import Database from '../../../lib/Database.js';
 
 export default async function handler(req, res) {
 	let db = new Database();
-	console.log(req);
-	let pkgName = req.path.split('/').pop();
+	let pkgName = req.url.split('/').pop();
 	let id = await db.getPackage(pkgName);
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'application/json');
