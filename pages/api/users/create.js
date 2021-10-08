e21e271bcc2d1be13fcdbb9278441b704b7e9081
R0bl0x10501050 <R0bl0x10501050@gmail.com>
2021-10-07 18:14:55 -0700

    fixed key varName error

--- pages/api/users/create.js
+++ pages/api/users/create.js
@@ -3,8 +3,8 @@ import Database from '../../../lib/Database.js';
 export default async function handler(req, res) {
 	let db = new Database();
 	let { pkgName, id, user, key } = req.query;
-	let key = await db.registerUser(user);
+	let newKey = await db.registerUser(user);
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'application/json');
-	res.end(JSON.stringify({ key: key }));
+	res.end(JSON.stringify({ key: newKey }));
 }
