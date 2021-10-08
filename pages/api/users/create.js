99ddf7556d709a4d84db400079d9fbb9804bad8d
R0bl0x10501050 <R0bl0x10501050@gmail.com>
2021-10-07 18:11:40 -0700

    added return value to db.registerUser

--- pages/api/users/create.js
+++ pages/api/users/create.js
@@ -3,8 +3,8 @@ import Database from '../../../lib/Database.js';
 export default async function handler(req, res) {
 	let db = new Database();
 	let { pkgName, id, user, key } = req.query;
-	await db.registerUser(user);
+	let key = await db.registerUser(user);
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'application/json');
-	res.end(JSON.stringify({}));
+	res.end(JSON.stringify({ key: key }));
 }
