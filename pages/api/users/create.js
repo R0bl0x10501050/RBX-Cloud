1a061f307ebd39aaac707e0debc8f95b977e3eb0
R0bl0x10501050 <R0bl0x10501050@gmail.com>
2021-10-07 17:36:24 -0700

    Adde users endpoints

---
+++ pages/api/users/create.js
@@ -0,0 +1,10 @@
+import Database from '../../../lib/Database.js';
+
+export default async function handler(req, res) {
+	let db = new Database();
+	let { pkgName, id, user, key } = req.query;
+	await db.registerUser(user);
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'application/json');
+	res.end(JSON.stringify({}));
+}
